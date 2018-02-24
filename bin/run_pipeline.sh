b65d0da1a97a294aace2604c87a78ed01b7d97d1
Alexandar Mechev <apmechev@gmail.com>
2018-02-24 16:07:40 +0100

    First integrated test sandbox

---
+++ bin/run_pipeline.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function run_pipeline(){
+
+
+echo ""
+echo "Running test script"
+$OLD_PYTHON update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'running pipeline'
+$OLD_PYTHON update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
+
+ls ${PWD}
+ls ${RUNDIR}/Input
+
+echo ""
+echo "Testing LOFAR Environment"
+which NDPPP
+NDPPP --version
+
+
+$OLD_PYTHON update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'processing_finished'
+
+}
