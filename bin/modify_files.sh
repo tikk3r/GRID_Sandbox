a871c9b82c3663a9d70096af50a6419a51091dd7
Alexandar Mechev <apmechev@gmail.com>
2018-02-28 14:26:29 +0100

    Modifying (maybe not completely) the parset to include input and output dirs

---
+++ bin/modify_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+function replace_dirs(){
+
+ sed -i "s?PREFACTOR_SCRATCH_DIR?$(pwd)?g" ${PARSET}
+ sed -i "s?msin\ .*?msin=$(pwd)/Input/*MS?g" ${PARSET}
+ sed -i "s?msout\ .*?msout=$(pwd)/Output/*MS?g" ${PARSET}
+
+ echo "Pipeline type is "$pipelinetype
+ echo "Adding $OBSID and $pipelinetype into the tcollector tags"
+ sed -i "s?\[\]?\[\ \"obsid=${OBSID}\",\ \"pipeline=${pipelinetype}_{$OBSID}\"\]?g" tcollector/collectors/etc/config.py
+
+}
