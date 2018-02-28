f7d5eac8189cae64fbc2a95ec7b90d2833c3b0dc
Alexandar Mechev <apmechev@gmail.com>
2018-02-28 15:17:05 +0100

    Updating Output MS name

--- bin/modify_files.sh
+++ bin/modify_files.sh
@@ -5,7 +5,7 @@ function replace_dirs(){
 
  sed -i "s?PREFACTOR_SCRATCH_DIR?$(pwd)?g" ${PARSET}
  sed -i "s?msin\ .*?msin=$(pwd)/Input/*MS?g" ${PARSET}
- sed -i "s?msout\ .*?msout=$(pwd)/Output/results_{$OBSID}.MS?g" ${PARSET}
+ sed -i "s?msout\ .*?msout=$(pwd)/Output/results_${OBSID}.MS?g" ${PARSET}
 
  echo "Pipeline type is "$pipelinetype
  echo "Adding $OBSID and $pipelinetype into the tcollector tags"
