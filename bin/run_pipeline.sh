7a17b5595d99d12fa80aaf46e7077b80299c6ad7
Alexandar Mechev <apmechev@gmail.com>
2018-03-06 14:34:02 +0100

    Adding generic pipeline parset to prefactor run_ppipeline.sh

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -5,14 +5,12 @@ function run_pipeline(){
 
 echo ""
 echo "Running test script"
-$OLD_PYTHON update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'running pipeline'
-$OLD_PYTHON update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
+python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'running pipeline'
+python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
 
-echo ""
-echo "Testing LOFAR Environment"
-NDPPP --version
-NDPPP ${PARSET}
+genericpipeline.py ${PWD}/${PARSET} -d -c pipeline.cfg > output
 
-$OLD_PYTHON update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'processing_finished'
+
+python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'processing_finished'
 
 }
