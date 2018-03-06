79a801103d5e16eebdec4c617c1fb7a611f09b17
Alexandar Mechev <apmechev@gmail.com>
2018-03-06 14:45:19 +0100

    Updating run_pipeline_script

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -4,13 +4,17 @@ function run_pipeline(){
 
 
 echo ""
-echo "Running test script"
-python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'running pipeline'
-python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
+echo "Running Prefactor Parset"
+#python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'running pipeline'
+#python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
 
+which genericpipeline.py
+echo ${PARSET}
+ls pipeline.cfg
 genericpipeline.py ${PWD}/${PARSET} -d -c pipeline.cfg > output
 
-
-python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'processing_finished'
+echo "Output file:"
+wc output
+#python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'processing_finished'
 
 }
