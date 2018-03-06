1a07d78f2d9836fc0c4c413df3e646b187804844
Alexandar Mechev <apmechev@gmail.com>
2018-03-06 12:08:49 +0100

    Making output of script clearer

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -17,11 +17,16 @@ which NDPPP
 NDPPP --version
 
 echo "Running script $SCRIPT"
-
+echo ""
+echo "--------------------------------"
+echo ""
 chmod a+x $SCRIPT
 
 ./${SCRIPT}
 
+echo ""
+echo "--------------------------------"
+echo ""
 $OLD_PYTHON update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'processing_finished'
 
 }
