0e77abb4cff7204f4bc4ffab0fed994862dd9ac3
Alexandar Mechev <apmechev@gmail.com>
2018-03-06 11:25:46 +0100

    Moving update_tok_freq from launcher to Snadbox

---
+++ update_token_freq.py
@@ -0,0 +1,29 @@
+#=========
+#Updates the status of the token from a shell script or another python script
+#
+#=========
+
+from GRID_PiCaS_Launcher  import couchdb
+import os,sys,time
+
+def update_freq(p_db,p_usr,p_pwd,tok_id,freq):
+    try:
+        server = couchdb.Server(url="https://picas-lofar.grid.sara.nl:6984")
+        server.resource.credentials = (p_usr,p_pwd)
+        db = server[p_db]
+    except couchdb.http.ServerError:
+        time.sleep(1)
+        update_freq(p_db,p_usr,p_pwd,tok_id,freq)
+
+    
+    token=db[tok_id] 
+    A_SBN=int(round((( ( float(freq)/1e6 - 100.0 ) / 100.0 ) * 512.0),0))
+    if 'FREQ' in token.keys():
+        token['FREQ']=freq
+    token['ABN']=A_SBN
+    db.update([token]) 
+    print(A_SBN)
+
+if __name__ == '__main__':
+    update_freq(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
+
