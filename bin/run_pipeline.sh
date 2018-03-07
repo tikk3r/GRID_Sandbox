168865fa51cbd3098f6a3c5f918277364e989e83
Alexandar Mechev <apmechev@gmail.com>
2018-03-07 16:12:29 +0100

    run_pipline includes ddf-pipeline scritps in PYTHONPATH

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -9,6 +9,13 @@ echo "Running Prefactor Parset"
 #python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
 
 
+export PYTHONPATH=${RUNDIR}/ddf-pipeline/utils/:$PYTHONPATH
+export PATH=${RUNDIR}/ddf-pipeline/utils/:$PATH
+
+export PYTHONPATH=${RUNDIR}/ddf-pipeline/scripts/:$PYTHONPATH
+export PATH=${RUNDIR}/ddf-pipeline/scripts/:$PATH
+
+
 
 ./ddf-pipeline/scripts/make_mslists.py
 python ddf-pipeline/scripts/pipeline.py ${PARSET} &> output
