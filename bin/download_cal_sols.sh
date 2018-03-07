8c35a27c4366a0bbce6513cec59d8683aef0b965
Alexandar Mechev <apmechev@gmail.com>
2018-03-07 16:14:02 +0100

    Adding cal solutions from proper place

--- bin/download_cal_sols.sh
+++ bin/download_cal_sols.sh
@@ -3,7 +3,7 @@
 function download_cals(){
 
  echo "Getting solutions from obsid " $1
- globus-url-copy gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/spectroscopy-migrated/prefactor/cal_sols/${1}_solutions.tar file:`pwd`/cal_solutions.tar
+ globus-url-copy gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/pipelines/SKSP/pref_cal2/${1}/${1}.tar file:`pwd`/cal_solutions.tar
  wait
  if [[ -e cal_solutions.tar ]]
   then
