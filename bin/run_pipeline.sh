a7f9859d1a7b4d8d2587fd4dd0bcc3ceff96bf84
Alexandar Mechev <apmechev@gmail.com>
2018-03-07 16:20:31 +0100

    Moving files to  explicitly

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -16,6 +16,7 @@ export PYTHONPATH=${RUNDIR}/ddf-pipeline/scripts/:$PYTHONPATH
 export PATH=${RUNDIR}/ddf-pipeline/scripts/:$PATH
 
 
+mv ${RUNDIR}/Input/prefactor/results/* ${RUNDIR}/
 
 ./ddf-pipeline/scripts/make_mslists.py
 python ddf-pipeline/scripts/pipeline.py ${PARSET} &> output
