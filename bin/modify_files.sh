aa14f68337cea34b0c0bd2cbb1560a25b7a267a0
Alexandar Mechev <apmechev@gmail.com>
2018-03-12 10:52:16 +0100

    Adding cat parset

--- bin/modify_files.sh
+++ bin/modify_files.sh
@@ -5,10 +5,10 @@ function replace_dirs(){
 
  sed -i "s?PREFACTOR_SCRATCH_DIR?$(pwd)?g" ${PARSET}
  if [ -f $(pwd)/Input/*MS ]; then
- sed -i "s?msin\ .*?msin=$(pwd)/Input/*MS?g" ${PARSET}
+     sed -i "s?msin\ .*?msin=$(pwd)/Input/*MS?g" ${PARSET}
  fi
  if [ -f $(pwd)/Input/*ms  ]; then
-      sed -i "s?msin\ .*?msin=$(pwd)/Input/*ms?g" ${PARSET}
+    sed -i "s?msin\ .*?msin=$(pwd)/Input/*ms?g" ${PARSET}
  fi
  sed -i "s?msout\ .*?msout=$(pwd)/Output/results_${OBSID}.MS?g" ${PARSET}
 
@@ -16,4 +16,6 @@ function replace_dirs(){
  echo "Adding $OBSID and $PIPELINE_STEP into the tcollector tags"
  sed -i "s?\[\]?\[\ \"obsid=${OBSID}\",\ \"pipeline=${PIPELINE_STEP}_{$OBSID}\"\]?g" tcollector/collectors/etc/config.py
 
+ echo "Modified Parset is:"
+ cat ${PARSET}
 }
