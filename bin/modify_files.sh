72c4751c5ab2e89505283156e0b379581be0d5a1
Alexandar Mechev <apmechev@gmail.com>
2018-03-12 10:42:23 +0100

    Modify files now adds .ms to parset

--- bin/modify_files.sh
+++ bin/modify_files.sh
@@ -4,7 +4,12 @@
 function replace_dirs(){
 
  sed -i "s?PREFACTOR_SCRATCH_DIR?$(pwd)?g" ${PARSET}
+ if [ -f $(pwd)/Input/*MS ]; then
  sed -i "s?msin\ .*?msin=$(pwd)/Input/*MS?g" ${PARSET}
+ fi
+ if [ -f $(pwd)/Input/*ms  ]; then
+      sed -i "s?msin\ .*?msin=$(pwd)/Input/*ms?g" ${PARSET}
+ fi
  sed -i "s?msout\ .*?msout=$(pwd)/Output/results_${OBSID}.MS?g" ${PARSET}
 
  echo "Pipeline Step is "$PIPELINE_STEP
