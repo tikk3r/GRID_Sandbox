fc644db82e8a14f456ef0d157351844084205eed
apmechev <apmechev@gmail.com>
2018-03-13 19:18:30 +0100

    Updating targ2 process to respect Input directory

--- bin/targ2_process.sh
+++ bin/targ2_process.sh
@@ -2,7 +2,7 @@
 
 function runtaql(){
 
-    echo "running taql on "$( ls -d *${OBSID}*SB*  )"/SPECTRAL_WINDOW"
+    echo "running taql on "$( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )"/SPECTRAL_WINDOW"
     FREQ=$( echo "select distinct REF_FREQUENCY from $( ls -d *${OBSID}*SB* )/SPECTRAL_WINDOW"| taql | tail -2 | head -1)
     A_SBN=$( $OLD_PYTHON update_token_freq.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} ${FREQ} )
     echo "Frequency is "${FREQ}" and Absolute Subband is "${A_SBN}
