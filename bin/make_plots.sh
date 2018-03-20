7f234156c301a581390ccc42f79190b7e31a0d76
Alexandar Mechev <apmechev@gmail.com>
2018-03-20 16:31:04 +0100

    some cleanup of make_plots

--- bin/make_plots.sh
+++ bin/make_plots.sh
@@ -1,27 +1,28 @@
 function make_pie(){
-
-xmlfile=$( find . -name "*statistics.xml" 2>/dev/null)
-#cp piechart/autopie.py .
-./autopie.py ${xmlfile} PIE_${OBSID}_${PIPELINE}.png
-cp $! ${WORKDIR}
-
+   
+   xmlfile=$( find . -name "*statistics.xml" 2>/dev/null)
+   #cp piechart/autopie.py .
+   ./autopie.py ${xmlfile} PIE_${OBSID}_${PIPELINE}.png
+   cp $! ${WORKDIR}
+   
 }
 
 function make_plots(){
-
-if [[ ! -z $( echo $pipelinetype |grep targ2 ) ]]
-  then
-    ./prefactor/scripts/plot_solutions_all_stations.py -p $( ls -d ${RUNDIR}/prefactor/results/*ms )/instrument_directionindependent/ ${JOBDIR}/GSM_CAL_${OBSID}_ABN${STARTSB}_plot.png
-fi
-
-xmlfile=$( find . -name "*statistics.xml" 2>/dev/null)
-cp piechart/autopie.py .
-./autopie.py ${xmlfile} PIE_${OBSID}.png
-
-find ${RUNDIR} -name "PIE*png"|xargs tar -zcf pngs.tar.gz
-find ${RUNDIR} -name "*.png" -exec cp {} ${JOBDIR} \;
-cp PIE_${OBSID}.png ${JOBDIR}
-cp ./prefactor/cal_results/*png ${JOBDIR}
-
+   
+   if [[ ! -z $( echo $pipelinetype |grep targ2 ) ]]
+     then
+       cd ${RUNDIR}
+       ./prefactor/scripts/plot_solutions_all_stations.py -p $( ls -d ${RUNDIR}/prefactor/results/*ms )/instrument_directionindependent/ ${JOBDIR}/GSM_CAL_${OBSID}_ABN${STARTSB}_plot.png
+   fi
+   
+   xmlfile=$( find . -name "*statistics.xml" 2>/dev/null)
+   cp piechart/autopie.py .
+   ./autopie.py ${xmlfile} PIE_${OBSID}.png
+   
+   find ${RUNDIR} -name "PIE*png"|xargs tar -zcf pngs.tar.gz
+   find ${RUNDIR} -name "*.png" -exec cp {} ${JOBDIR} \;
+   cp PIE_${OBSID}.png ${JOBDIR}
+   cp ./prefactor/cal_results/*png ${JOBDIR}
+   
 }
 
