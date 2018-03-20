6df694d2bd629ff231c3863dad328d168880022d
Alexandar Mechev <apmechev@gmail.com>
2018-03-20 16:26:52 +0100

    Adding back the update_token_status which looks at the current prefactor step and puts it into the Token

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -5,8 +5,8 @@ function run_pipeline(){
 
 echo ""
 echo "Running Prefactor Parset"
-#python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'running pipeline'
-#python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
+python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'running pipeline'
+python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
 
 which genericpipeline.py
 echo ${PARSET}
