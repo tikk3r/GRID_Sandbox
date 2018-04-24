1cf62544bbe1b528e5de263871975243f3202ba5
Alexandar Mechev <apmechev@gmail.com>
2018-04-24 13:17:22 +0200

    Updating pre-cal ms flag

--- bin/download.sh
+++ bin/download.sh
@@ -84,7 +84,7 @@ function dl_targ2(){
    sed 's?srm://srm.grid.sara.nl:8443?gsiftp://gridftp.grid.sara.nl:2811?g' $1 | xargs -I{} globus-url-copy -st 30 -fast -v {} ${RUNDIR}/Input/ || { echo 'downloading failed' ; exit 21;  }
    ls 
    for i in `ls *tar`; do tar -xf $i  && rm -rf $i; done
-   find . -type d -name "*uv.pre-cal.ms" -exec mv {} ./ \;
+   find . -type d -name "*.pre-cal.ms" -exec mv {} ./ \;
    echo "Input directory size:"
    du -hs .
    du -hs scratch
