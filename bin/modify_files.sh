0a99e1432af833840ae12c012410ac771de53436
Alexandar Mechev <apmechev@gmail.com>
2018-05-28 11:24:49 +0200

    adding parset modifications

--- bin/modify_files.sh
+++ bin/modify_files.sh
@@ -3,7 +3,6 @@
 
 function replace_dirs(){
 
- sed -i "s?PREFACTOR_SCRATCH_DIR?$(pwd)?g" ${PARSET}
  if [ -d $(pwd)/Input/*MS ]; then
      sed -i "s?msin\ .*?msin=$(pwd)/Input/*MS?g" ${PARSET}
  fi
@@ -12,10 +11,8 @@ function replace_dirs(){
  fi
  sed -i "s?msout\ .*?msout=$(pwd)/Output/results_${PIPELINE_STEP}_${OBSID}_SB${STARTSB}.MS?g" ${PARSET}
 
- echo "Pipeline Step is "$PIPELINE_STEP
- echo "Adding $OBSID and $PIPELINE_STEP into the tcollector tags"
- sed -i "s?\[\]?\[\ \"obsid=${OBSID}\",\ \"pipeline=${PIPELINE_STEP}_{$OBSID}\"\]?g" tcollector/collectors/etc/config.py
 
  echo "Modified Parset is:"
  cat ${PARSET}
+ echo ""
 }
