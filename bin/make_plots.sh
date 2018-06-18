9843f87c67f7e047795ad8d980cda780f64f6e5d
Alexandar Mechev <apmechev@gmail.com>
2018-06-18 16:03:36 +0200

    trimming 'make_plots()'

--- bin/make_plots.sh
+++ bin/make_plots.sh
@@ -15,13 +15,13 @@ function make_plots(){
        ./prefactor/scripts/plot_solutions_all_stations.py -p $( ls -d ${RUNDIR}/prefactor/results/*ms )/instrument_directionindependent/ ${JOBDIR}/GSM_CAL_${OBSID}_ABN${STARTSB}_plot.png
    fi
    
-   xmlfile=$( find . -name "*statistics.xml" 2>/dev/null)
-   cp piechart/autopie.py .
-   ./autopie.py ${xmlfile} PIE_${OBSID}.png
-   
-   find ${RUNDIR} -name "PIE*png"|xargs tar -zcf pngs.tar.gz
+#   xmlfile=$( find . -name "*statistics.xml" 2>/dev/null)
+#   cp piechart/autopie.py .
+#   ./autopie.py ${xmlfile} PIE_${OBSID}.png
+#   
+#   find ${RUNDIR} -name "PIE*png"|xargs tar -zcf pngs.tar.gz
    find ${RUNDIR} -name "*.png" -exec cp {} ${JOBDIR} \;
-   cp PIE_${OBSID}.png ${JOBDIR}
+#  cp PIE_${OBSID}.png ${JOBDIR}
    cp ./prefactor/cal_results/*png ${JOBDIR}
    
 }
