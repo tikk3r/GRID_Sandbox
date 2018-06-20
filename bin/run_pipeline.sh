f29a37edd059b047ba257b1d3478ef829119cb6e
Alexandar Mechev <apmechev@gmail.com>
2018-06-20 12:10:56 +0200

    more timestamps

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -5,13 +5,16 @@ function run_pipeline(){
 
 echo ""
 echo "Running Prefactor Parset"
-python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'running pipeline'
+python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'launching_pipeline'
 python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
 
 which genericpipeline.py
 echo ${PARSET}
 ls pipeline.cfg
 genericpipeline.py ${PWD}/${PARSET} -d -c pipeline.cfg > output
+
+
+python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'pipeline_completed'
 
 echo "Output file:"
 wc output
