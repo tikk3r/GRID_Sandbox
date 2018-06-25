eb83165aa336072ebe4452a6da68c2149db6c9a5
Alexandar Mechev <apmechev@gmail.com>
2018-06-25 11:00:21 +0200

    update to plots

--- bin/make_plots.sh
+++ bin/make_plots.sh
@@ -10,20 +10,12 @@ function make_pie(){
 function make_plots(){
    python  ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'making_plots'
 
-   if [[ ! -z $( echo $pipelinetype |grep targ2 ) ]]
+   if [[ ! -z $( echo $PIPELINE_STEP |grep targ2 ) ]]
      then
        cd ${RUNDIR}
        ./prefactor/scripts/plot_solutions_all_stations.py -p $( ls -d ${RUNDIR}/prefactor/results/*ms )/instrument_directionindependent/ ${JOBDIR}/GSM_CAL_${OBSID}_ABN${STARTSB}_plot.png
    fi
-   
-#   xmlfile=$( find . -name "*statistics.xml" 2>/dev/null)
-#   cp piechart/autopie.py .
-#   ./autopie.py ${xmlfile} PIE_${OBSID}.png
-#   
-#   find ${RUNDIR} -name "PIE*png"|xargs tar -zcf pngs.tar.gz
    find ${RUNDIR} -name "*.png" -exec cp {} ${JOBDIR} \;
-#  cp PIE_${OBSID}.png ${JOBDIR}
-   cp ${RUNDIR}/prefactor/cal_results/*png ${JOBDIR}
-   
+  
 }
 
