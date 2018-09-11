e8a9e5345c2ee83b99ef1459153acf45b54323aa
Kimberly Emig <kimberly@loui.grid.surfsara.nl>
2018-09-11 09:29:22 +0200

    save copy of plots from JOBDIR to RUNDIR

--- bin/disc_funcs.sh
+++ bin/disc_funcs.sh
@@ -134,7 +134,7 @@ function upload_trg1(){
 
 function save_plots(){
     echo "Saving plots"
-    find . -name "*.png" -exec cp {} ${JOBDIR} \;
+    find . -name "*.png" -exec cp {} ${RUNDIR} \;
 }
 
 
