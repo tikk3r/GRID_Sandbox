f9066ef47aa8ad52aa566a4822f2a0292c1d1e48
Alexandar Mechev <apmechev@gmail.com>
2018-09-11 11:04:12 +0200

    adding singularity switch in case  exists

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-function run_pipeline(){
 
+function run_with_singularity(){
+# first argument is the singularity Image file
+# second argument is the parset to run
+echo $1 $2
+
+singularity exec $1 which NDPPP
+singularity exec $1 NDPPP -v
+singularity exec $1 NDPPP $2
+
+
+}
+
+function run_pipeline(){
 
 echo ""
 echo "Running test script"
@@ -12,10 +24,17 @@ $OLD_PYTHON update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} $
 #echo "Testing LOFAR Environment"
 #source /cvmfs/softdrive.nl/lofar_sw/factor_prereqs/dysco/init_env.sh
 
-SINGULARITY_IMAGE=/cvmfs/softdrive.nl/fsweijen/singularity/lofar_3_1_4.simg
-echo "Using singularity image at $SINGULARITY_IMAGE"
-singularity exec $SINGULARITY_IMAGE NDPPP -v
-singularity exec $SINGULARITY_IMAGE NDPPP ${PARSET}
+#SINGULARITY_IMAGE=/cvmfs/softdrive.nl/fsweijen/singularity/lofar_3_1_4.simg
+
+
+if [[ -n $SIMG   ]]; then
+    echo "Using singularity image at $SIMG"
+    run_with_singularity $SIMG $SCRIPT
+  else
+    which NDPPP
+    NDPPP -v
+    NDPPP ${PARSET}
+fi
 
 $OLD_PYTHON update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'processing_finished'
 
