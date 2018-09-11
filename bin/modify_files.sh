16c0aab625e1a49856fea9e70846c4eaa37e7118
Alexandar Mechev <apmechev@gmail.com>
2018-09-11 10:59:20 +0200

    prints out error if no input files found

--- bin/modify_files.sh
+++ bin/modify_files.sh
@@ -5,9 +5,10 @@ function replace_dirs(){
 
  if [ -d $(pwd)/Input/*MS ]; then
      sed -i "s?msin\ .*?msin=$(pwd)/Input/*MS?g" ${PARSET}
- fi
- if [ -d $(pwd)/Input/*ms  ]; then
+ elif [ -d $(pwd)/Input/*ms  ]; then
     sed -i "s?msin\ .*?msin=$(pwd)/Input/*ms?g" ${PARSET}
+ else
+     echo "NO INPUT FILE FOUND in $(pwd)/Input/*.{MS,ms}"
  fi
  sed -i "s?msout\ .*?msout=$(pwd)/Output/results_${PIPELINE_STEP}_${OBSID}_SB${STARTSB}.MS?g" ${PARSET}
 
