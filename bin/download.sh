5ce51c4eed884f215a8abbc656871c14d382bb1e
Alexandar Mechev <apmechev@gmail.com>
2018-11-26 15:09:08 +0100

    adding max wait time

--- bin/download.sh
+++ bin/download.sh
@@ -44,15 +44,15 @@ function dl_targ1(){
 function dl_cal1(){
 
    if [[ ! -z $( cat $1 | grep juelich )  ]]; then 
-     sed 's?srm://lofar-srm.fz-juelich.de:8443?gsiftp://lofar-gridftp.fz-juelich.de:2811?g' $1 | xargs -I{} globus-url-copy -rst -st 30 -fast -v {} $PWD/Input/ || { echo 'downloading failed' ; exit 21; }
+     sed 's?srm://lofar-srm.fz-juelich.de:8443?gsiftp://lofar-gridftp.fz-juelich.de:2811?g' $1 | xargs -I{} globus-url-copy -rst -rst-timeout 1200 -st 30 -fast -v {} $PWD/Input/ || { echo 'downloading failed' ; exit 21; }
    fi
 
    if [[ ! -z $( cat $1 | grep sara )  ]]; then
-     sed 's?srm://srm.grid.sara.nl:8443?gsiftp://gridftp.grid.sara.nl:2811?g' $1 | xargs -I{} globus-url-copy -rst -st 30 -fast -v {} $PWD/Input/ || { echo 'downloading failed' ; exit 21; }
+     sed 's?srm://srm.grid.sara.nl:8443?gsiftp://gridftp.grid.sara.nl:2811?g' $1 | xargs -I{} globus-url-copy -rst -rst-timeout 1200 -st 30 -fast -v {} $PWD/Input/ || { echo 'downloading failed' ; exit 21; }
    fi
 
    if [[ ! -z $( cat $1 | grep psnc )  ]]; then
-     sed 's?srm://lta-head.lofar.psnc.pl:8443?gsiftp://gridftp.lofar.psnc.pl:2811?g' $1 | xargs -I{} globus-url-copy  -rst -st 30 -fast -v {} $PWD/Input/ || { echo 'downloading failed' ; exit 21; }
+     sed 's?srm://lta-head.lofar.psnc.pl:8443?gsiftp://gridftp.lofar.psnc.pl:2811?g' $1 | xargs -I{} globus-url-copy  -rst -rst-timeout 1200 -st 30 -fast -v {} $PWD/Input/ || { echo 'downloading failed' ; exit 21; }
    fi
 
    wait
