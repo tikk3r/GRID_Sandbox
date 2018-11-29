0e5e4126099e98880bb653c9640cedfca4f3a70f
Alexandar Mechev <apmechev@gmail.com>
2018-11-29 10:52:57 +0100

    making prefactor work with Singularity

--- bin/run_pipeline.sh
+++ bin/run_pipeline.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-function run_pipeline(){
 
+
+function run_with_singularity(){
+# first argument is the singularity Image file
+# second argument is the script to run
+
+echo "Running prefactor in image ${SIMG}"
+
+singularity exec ${SIMG} which genericpipeline.py
+echo ${PARSET}
+cat sing_pipeline.cfg
+
+singularity exec ${SIMG} genericpipeline.py ${PWD}/${PARSET} -d -c sing_pipeline.cfg  > output
+}
+
+function run_pipeline(){
 
 echo ""
 echo "Running Prefactor Parset"
 python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'launching_pipeline'
 python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_progress.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} output ${PARSET} &
 
-which genericpipeline.py
-echo ${PARSET}
-ls pipeline.cfg
-genericpipeline.py ${PWD}/${PARSET} -d -c pipeline.cfg > output
 
+if [[ -n $SIMG  ]]; then
+    echo "Running using the singularity image at ${SIMG}"
+    run_with_singularity 
+else
+    echo "Running using LOFAR version at $LOFAR_PATH"
+    which genericpipeline.py
+    echo ${PARSET}
+    cat pipeline.cfg
+    genericpipeline.py ${PWD}/${PARSET} -d -c pipeline.cfg > output
+fi
 
 python ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'pipeline_completed'
 
