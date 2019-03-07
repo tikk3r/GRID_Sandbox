5d5f00714f3409355e849b7dce7963fdaf13f89e
Frits Sweijen <fritssweijen@hotmail.com>
2019-03-07 14:02:57 +0100

    Change download_sols to find h5 instead of npy.

--- bin/download_cal_sols.sh
+++ bin/download_cal_sols.sh
@@ -3,7 +3,7 @@
 function download_cals(){
 
  echo "Getting solutions from obsid " $1
- globus-url-copy gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/pipelines/SKSP/pref_cal2/${1}/${1}.tar file:${RUNDIR}/Input/cal_solutions.tar
+ globus-url-copy gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/pipelines/SKSP/pref3_cal/${1}/${1}.tar file:${RUNDIR}/Input/cal_solutions.tar
  wait
  cd ${RUNDIR}/Input
  if [[ -e cal_solutions.tar ]]
@@ -13,6 +13,6 @@ function download_cals(){
     exit 23 #exit 23=> numpy solutions do not get downloaded
  fi
  mkdir -p ${RUNDIR}/prefactor/cal_results
- find ${RUNDIR}/Input/ -name "*.npy" -exec mv {} ${RUNDIR}/prefactor/cal_results/ \; 
+ find ${RUNDIR}/Input/ -name "*.h5" -exec mv {} ${RUNDIR}/prefactor/cal_results/ \; 
  cd ${RUNDIR}
 }
