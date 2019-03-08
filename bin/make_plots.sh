21a30b1c88fcd39abb17e38ef2675915c156192f
Frits Sweijen <fritssweijen@hotmail.com>
2019-03-08 14:04:12 +0100

    Remove more pngs.

--- bin/make_plots.sh
+++ bin/make_plots.sh
@@ -16,8 +16,9 @@ function make_plots(){
        ./prefactor/scripts/plot_solutions_all_stations.py -p $( ls -d ${RUNDIR}/prefactor/results/*ms )/instrument_directionindependent/ ${JOBDIR}/GSM_CAL_${OBSID}_ABN${STARTSB}_plot.png
    fi
    #find ${RUNDIR}/prefactor/*result* -name "*.png" -exec cp {} ${JOBDIR} \;
-   # Remove prefactor docs so the don't get picked up
-   rm -rf ${JOBDIR}/prefactor/docs
+   # Remove pngs in prefactor docs and Input so they don't get picked up
+   find ${JOBDIR}/prefactor/ -name "*.png" -exec rm {} \;
+   find ${JOBDIR}/Input/ -name "*.png" -exec rm {} \;
    find ${RUNDIR}/Output/  -name "*.png" -exec cp {} ${JOBDIR} \;
 
    python  ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'copied_plots' 
