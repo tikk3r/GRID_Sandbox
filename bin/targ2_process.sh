275d1858bdccb7e1f7dab086cd8b63f38fca69b5
Alexandar Mechev <apmechev@gmail.com>
2019-03-21 11:41:29 +0100

    Adding sing to taql command

--- bin/targ2_process.sh
+++ bin/targ2_process.sh
@@ -2,12 +2,11 @@
 
 function runtaql(){
 
-    echo "running taql on "$( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )"/SPECTRAL_WINDOW"
-    FREQ=$( echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB* )/SPECTRAL_WINDOW"| taql | tail -1 | head -1)
+     echo "running taql on "$( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )"/SPECTRAL_WINDOW"
+    FREQ=$( singularity exec $SIMG echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB* )/SPECTRAL_WINDOW"| singularity exec $SIMG taql | tail -n 2 | head -n 1)
     A_SBN=$( python  update_token_freq.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} ${FREQ} )
     echo "Frequency is "${FREQ}" and Absolute Subband is "${A_SBN}
     mv prefactor/results/L*ms ${RUNDIR}  #moves untarred results from targ1 to ${RUNDIR} 
-
 
 
 }
