f5ca1687df2ed5fe5bacb9bda6d829358238879b
Alexandar Mechev <apmechev@gmail.com>
2019-03-26 11:08:21 +0100

    Update to use new pipelines_dir

--- bin/download_cal_sols.sh
+++ bin/download_cal_sols.sh
@@ -3,7 +3,7 @@
 function download_cals(){
 
  echo "Getting solutions from obsid " $1
- globus-url-copy gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/pipelines/SKSP/pref_cal2/${1}/${1}.tar file:${RUNDIR}/Input/cal_solutions.tar
+ globus-url-copy gsiftp://gridftp.grid.sara.nl:2811/pnfs/grid.sara.nl/data/lofar/user/sksp/diskonly/pipelines/SKSP/prefactor_v1.0/pref_cal2/${1}/${1}.tar file:${RUNDIR}/Input/cal_solutions.tar
  wait
  cd ${RUNDIR}/Input
  if [[ -e cal_solutions.tar ]]
