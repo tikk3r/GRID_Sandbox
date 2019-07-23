2a45c3d335b0335dc356f8b29e0d6639a102bb7d
kimager <kimberly.emig@gmail.com>
2019-07-23 12:19:30 +0200

    update files to stores

--- bin/cygx_funcs.sh
+++ bin/cygx_funcs.sh
@@ -72,10 +72,8 @@ function upload_cygx_cal1(){
     #cd ${RUNDIR}/Output
 
     python  ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'archiving results'
-    find . -name instrument |xargs tar -cvf results.tar
-    find *MS.fac/ANTENNA |xargs tar -rvf results.tar
-    find *MS.fac/FIELD |xargs tar -rvf results.tar
-    find *MS.fac/sky |xargs tar -rvf results.tar 
+    find . -name instrument*.h5 |xargs tar -cvf results.tar
+    find *fits |xargs tar -rvf results.tar 
 
     python  ${JOBDIR}/GRID_PiCaS_Launcher/update_token_status.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} 'uploading results'
     globus-url-copy results.tar ${RESULTS_DIR}/${OBSID}/cal1_SB${STARTSB}.tar || { echo "Upload Failed"; exit 31;} # exit 31 => Upload to storage failed
