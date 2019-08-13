bb5c6a8b585277242ae1a3e6dfbcf8e0852c3f6e
Alexandar Mechev <apmechev@gmail.com>
2019-08-13 15:22:56 +0200

    inverted logic

--- bin/targ2_process.sh
+++ bin/targ2_process.sh
@@ -3,7 +3,7 @@
 function runtaql(){
 
     echo "running taql on "$( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )"/SPECTRAL_WINDOW"
-    if  [  -z ${SIMG+x} ]; then
+    if  [ ! -z ${SIMG+x} ]; then
         FREQ=$( singularity exec $SIMG echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB* )/SPECTRAL_WINDOW"| singularity exec $SIMG taql | tail -n 2 | head -n 1)
     else
         FREQ=$( echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )/SPECTRAL_WINDOW"| taql | tail -1 | head -1)
