bb08627187025abba5d9a536b822299749a50993
Alexandar Mechev <apmechev@gmail.com>
2019-08-14 16:53:42 +0200

    Also using A_SBN

--- bin/targ2_process.sh
+++ bin/targ2_process.sh
@@ -8,6 +8,7 @@ function runtaql(){
     else
         FREQ=$( echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )/SPECTRAL_WINDOW"| taql | tail -1 | head -1)
     fi
+    export A_SBN=$( python  update_token_freq.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} ${FREQ} )
     export ABN=$( python  update_token_freq.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} ${FREQ} )
     echo "Frequency is "${FREQ}" and Absolute Subband is "${ABN}
     mv prefactor/results/L*ms ${RUNDIR}  #moves untarred results from targ1 to ${RUNDIR} 
