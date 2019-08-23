deebce48be853cec45f123395609598834bb4201
Frits Sweijen <fritssweijen@hotmail.com>
2019-08-23 11:02:11 +0200

    Bind PWD with Singularity when running TaQL.

--- bin/targ2_process.sh
+++ bin/targ2_process.sh
@@ -3,7 +3,7 @@
 function runtaql(){
 
     echo "running taql on "$( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )"/SPECTRAL_WINDOW"
-    FREQ=$( singularity exec $SIMG echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB* )::SPECTRAL_WINDOW"| singularity exec $SIMG taql | tail -n 2 | head -n 1)
+    FREQ=$( singularity exec -B $PWD $SIMG echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB* )::SPECTRAL_WINDOW"| singularity exec -B $PWD $SIMG taql | tail -n 2 | head -n 1)
     export ABN=$( python  update_token_freq.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} ${FREQ} )
     echo "Frequency is "${FREQ}" and Absolute Subband is "${ABN}
     mv prefactor/results/L*ms ${RUNDIR}  #moves untarred results from targ1 to ${RUNDIR} 
