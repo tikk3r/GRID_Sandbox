be94067331145858d996764843cf6a64ac544fba
Frits Sweijen <fritssweijen@hotmail.com>
2019-08-23 14:25:26 +0200

    Trying to fix taql again.

--- bin/targ2_process.sh
+++ bin/targ2_process.sh
@@ -4,7 +4,7 @@ function runtaql(){
 
     ls ${RUNDIR}/Input/
     echo "running taql on "$( ls -d ${RUNDIR}/Input/*${OBSID}*SB*  )"/SPECTRAL_WINDOW"
-    FREQ=$( singularity exec -B /scratch,$PWD $SIMG echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB* )::SPECTRAL_WINDOW"| singularity exec -B /scratch,$PWD $SIMG taql | tail -n 2 | head -n 1)
+    FREQ=$( singularity -v exec -B /scratch,$PWD $SIMG echo "select distinct REF_FREQUENCY from $( ls -d ${RUNDIR}/Input/*${OBSID}*SB* )::SPECTRAL_WINDOW"| singularity -v exec -B /scratch,$PWD $SIMG taql | tail -n 2 | head -n 1)
     export ABN=$( python  update_token_freq.py ${PICAS_DB} ${PICAS_USR} ${PICAS_USR_PWD} ${TOKEN} ${FREQ} )
     echo "Frequency is "${FREQ}" and Absolute Subband is "${ABN}
     mv prefactor/results/L*ms ${RUNDIR}  #moves untarred results from targ1 to ${RUNDIR} 
